7d4cb8e763f7c626a13fb021d980ec34ab9b0707
Mohamed-Afzal <79313441+Mohamed-Afzal@users.noreply.github.com>
2024-02-24 15:16:44 +0530

    Stacked Dot Plot

---
+++ Stacked Dot Plot.R
@@ -0,0 +1,14 @@
+#Create a Stacked Dot Plot in R.
+
+# Load the necessary library
+library(ggplot2)
+
+# Use the mtcars dataset
+data(mtcars)
+
+# Create the plot
+ggplot(mtcars, aes(x=factor(cyl), y=mpg)) +
+  geom_dotplot(binaxis='y', stackdir='center', dotsize = 0.5) +
+  theme_minimal() +
+  labs(x="Number of Cylinders", y="Miles Per Gallon", title="Stacked Dot Plot")
+
